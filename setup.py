fc33b44fdeecc18ca784d2dda0cf5028a4734a0c
Your Name <you@example.com>
2018-04-11 02:57:37 +0100

    install requires

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 setup(
     name='py_bandcamp',
-    version='0.4',
+    version='0.4.1',
     packages=['py_bandcamp'],
-    install_requires = ["bs4", "demjson"],
+    install_requires = ["bs4", "demjson", "lxml"],
     url='https://github.com/JarbasAl/py_bandcamp',
     license='MIT',
     author='jarbasAI',
